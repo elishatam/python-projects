61e04af8d1c9428db53b78f329dd58337c674056
Elisha Tam <elisha.tam@gmail.com>
2018-04-04 14:52:41 -0700

    Update with exercises from Python courses

--- Learning Python/Ex_Files_Learning_Python/Exercise Files/Ch2/classes_start.py
+++ Learning Python/Ex_Files_Learning_Python/Exercise Files/Ch2/classes_start.py
@@ -2,9 +2,34 @@
 # Example file for working with classes
 #
 
+class myClass():
+    #self = refers to object itself
+    #First argument of a method of a class is self
+    def method1(self):
+        print("myClass method1")
+        
+    def method2(self, someString):
+        print("myClass method2 " + someString)
 
+#Inherit from myClass
+class anotherClass(myClass):
+    #self = refers to object itself
+    #First argument of a method of a class is self
+    def method1(self):
+        myClass.method1(self)  #call the inherited method on the super class
+        print("anotherClass method1")
+        
+    def method2(self, someString):
+        print("anotherClass method2 ")
 def main():
-
+    #instantiate class
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
   
+    c2=anotherClass()
+    c2.method1()    #call the inherited method on the super class
+    c2.method2("This is a string")
+    
 if __name__ == "__main__":
   main()
---
+++ Learning Python/LearningPython.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 16:30:19 2018
+
+@author: elishatam
+"""
+
+f="abc"
+print(f+str(123))
