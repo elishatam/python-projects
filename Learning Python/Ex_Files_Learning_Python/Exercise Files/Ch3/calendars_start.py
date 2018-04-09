8ce84bbdeed27abfb81251de3e51fe3da04d05de
Elisha Tam <elisha.tam@gmail.com>
2018-04-09 15:48:49 -0700

    Finished Learning Python LinkedIn class

--- Learning Python/Ex_Files_Learning_Python/Exercise Files/Ch3/calendars_start.py
+++ Learning Python/Ex_Files_Learning_Python/Exercise Files/Ch3/calendars_start.py
@@ -6,8 +6,8 @@
 import calendar
 
 # create a plain text calendar
-c = calendar.TextCalendar(calendar.SUNDAY)
-st = c.formatmonth(2017, 1, 0, 0)
+c = calendar.TextCalendar(calendar.MONDAY)
+st = c.formatmonth(2018, 1, 0, 0)
 print(st)
 
 # create an HTML formatted calendar
@@ -22,14 +22,26 @@ print(st)
   
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
-for name in calendar.month_name:
-    print(name)
-    
-for day in calendar.day_name:
-    print(day)
+#for name in calendar.month_name:
+#    print(name)
+#    
+#for day in calendar.day_name:
+#    print(day)
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
-  
+print("Team meetings will be on: ")
+for m in range(1,13): #13 is non-inclusive in range
+    cal = calendar.monthcalendar(2018, m)
+    print cal
+    weekone=cal[0]
+    weektwo=cal[1]
+    
+    if weekone[calendar.FRIDAY] != 0: 
+        meetday = weekone[calendar.FRIDAY] #means Friday is in first week
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+                          
+    print("%10s %2d" % (calendar.month_name[m], meetday))
