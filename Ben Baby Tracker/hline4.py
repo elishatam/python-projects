3b184ab27bc30c88c7bc4ba94ebc500250e4f2ed
Elisha Tam <elisha.tam@gmail.com>
2019-04-03 22:53:00 -0700

    Fix same date bug

--- Ben Baby Tracker/hline4.py
+++ Ben Baby Tracker/hline4.py
@@ -2,8 +2,9 @@ from datetime import datetime, date
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as dtm
+import matplotlib.patches as mpatches
 
-df = pd.read_csv('firstmonth.csv')
+df = pd.read_csv('2weeks.csv')
 
 #Pandas to_datetime() converts string Date time into Python date time object
 df.Time = pd.to_datetime(df.Time)  #dtype: datetime64[ns]. Matplotlib can't plot this datatype
@@ -20,9 +21,16 @@ df['DateString'] = df['DateOnly'].astype('|S')
 #Make the same dummy date for the date time so hlines will stack
 df['Time_SameDate']=df.Time.map(lambda t: t.replace(year=2019, month=1, day=1)) #https://stackoverflow.com/questions/17152719/change-date-of-a-datetimeindex
 df['EndTime_SameDate']=df.EndTime.map(lambda t: t.replace(year=2019, month=1, day=1))
-#df.info()
-#df
-#print(df)
+
+df['Daydifference']=df.DateOnly - date(2017,8,2)
+#for index, row in df.iterrows():
+#	df['Time_SameDate2']=df.Time - pd.to_timedelta('1 days')
+df['Time_SameDate2']=df.Time - df.Daydifference
+df['EndTime_SameDate2']=df.EndTime - df.Daydifference
+df.info()
+df
+print(df)
+
 
 #Need to change dtype to datetime.date instances
 #https://matplotlib.org/gallery/recipes/common_date_problems.html
@@ -30,6 +38,8 @@ df['EndTime_SameDate']=df.EndTime.map(lambda t: t.replace(year=2019, month=1, da
 #df.EndTime = df.EndTime.astype(datetime)
 df.Time_SameDate = df.Time_SameDate.astype(datetime)
 df.EndTime_SameDate = df.EndTime_SameDate.astype(datetime)
+df.Time_SameDate2 = df.Time_SameDate2.astype(datetime)
+df.EndTime_SameDate2 = df.EndTime_SameDate2.astype(datetime)
 
 
 df.info()
@@ -52,16 +62,19 @@ colors = {
 	"BottlePumped": "green"
 }
 
-#myFmt = dtm.DateFormatter('%H:%M')
-#ax = ax.xaxis.set_major_formatter(myFmt)
-#ax = ax.xaxis_date()   #Change format later: https://stackoverflow.com/questions/14946371/editing-the-date-formatting-of-x-axis-tick-labels-in-matplotlib
-#ax = plt.hlines(df.Number, dt.date2num(df.Time), dt.date2num(df.endtime), linewidth=15, color=df.color)
-#ax = plt.hlines(df.Number, dt.date2num(df.Time), dt.date2num(df.endtime), linewidth=15, color=colors[(df.resource)])
-#ax = plt.hlines(df.Number, dt.date2num(df.Time), dt.date2num(df.endtime), linewidth=15, color=colors['Sleep'])
-#ax = plt.hlines(df.DateString, dtm.date2num(df.Time_SameDate), dtm.date2num(df.EndTime_SameDate), linewidth=15, color="red")
-ax = plt.hlines(df.DateString, dtm.date2num(df.Time_SameDate), dtm.date2num(df.EndTime_SameDate), linewidth=15, color=df['Resource'].map(colors))
+#ax = plt.hlines(df.DateString, dtm.date2num(df.Time_SameDate), dtm.date2num(df.EndTime_SameDate), linewidth=15, color=df['Resource'].map(colors))
+ax = plt.hlines(df.DateString, dtm.date2num(df.Time_SameDate2), dtm.date2num(df.EndTime_SameDate2), linewidth=15, color=df['Resource'].map(colors))
 
-plt.margins(.15)  #So xticks start at 12:00am
+#Make own legend: https://stackoverflow.com/questions/39500265/manually-add-legend-items-python-matplotlib
+#red_patch=mpatches.Patch(color='red', label='Sleep')
+sleep_patch=mpatches.Patch(color=colors["Sleep"], label='Sleep')
+nursing_patch=mpatches.Patch(color=colors["Nursing"], label='Nursing')
+diaper_patch=mpatches.Patch(color=colors["Wet"], label='Diaper')
+bottle_patch=mpatches.Patch(color=colors["BottleFormula"], label='Bottle')
+
+plt.legend(handles=[sleep_patch, nursing_patch, diaper_patch, bottle_patch]) #bbox_to_anchor=(0.1, 0)
+
+#plt.margins(.15)  #So xticks start at 12:00am
 plt.xticks(rotation=90)
 fig.tight_layout()  #Border will fit around axis
 plt.show()
